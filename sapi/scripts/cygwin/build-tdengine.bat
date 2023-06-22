5c6303dbea4798657d135b36f6f141dab5fe0280
wenjie <w@tryto.cn>
2023-06-22 13:34:15 +0800

    tdengine

---
+++ sapi/scripts/cygwin/build-tdengine.bat
@@ -0,0 +1,18 @@
+@ECHO OFF
+
+d:
+cd "D:\TDengine"
+
+mkdir release
+
+cd release
+
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" x64
+
+cmake .. -G "NMake Makefiles" -DBUILD_JDBC=false -DTD_BUILD_HTTP=false -DTD_BUILD_LUA=false
+
+nmake
+
+nmake install
+
+copy "C:\TDengine\driver\taos.dll" "C:\Windows\System32"
