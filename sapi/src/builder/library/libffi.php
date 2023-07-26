55c67aa063314b797122d24a1a79d84c8b19a856
wenjie <w@tryto.cn>
2023-07-26 13:12:38 +0800

    swow

--- sapi/src/builder/library/libffi.php
+++ sapi/src/builder/library/libffi.php
@@ -4,9 +4,7 @@ use SwooleCli\Library;
 use SwooleCli\Preprocessor;
 
 return function (Preprocessor $p) {
-
     $libffi_prefix = FFI_PREFIX;
-
     $p->addLibrary(
         (new Library('libffi'))
             ->withHomePage('https://sourceware.org/libffi/')
@@ -14,17 +12,11 @@ return function (Preprocessor $p) {
             ->withUrl('https://github.com/libffi/libffi/releases/download/v3.4.4/libffi-3.4.4.tar.gz')
             ->withFile('libffi-3.4.4.tar.gz')
             ->withPrefix($libffi_prefix)
-            ->withConfigure(
-            <<<EOF
-            ./configure --help
-            ./configure --prefix={$libffi_prefix} \
-            --enable-static=yes \
-            --enable-shared=no
-EOF
-            )
-            ->withPkgName('libffi')
-            ->withPkgConfig($libffi_prefix . '/lib/pkgconfig')
-            ->withLdflags('-L' . $libffi_prefix . '/lib')
+            ->withConfigure('./configure --prefix=' . $libffi_prefix . ' --enable_static=yes --enable_shared=no')
             ->withBinPath($libffi_prefix . '/bin/')
     );
+
+    $p->withVariable('CPPFLAGS', '$CPPFLAGS -I' . $libffi_prefix . '/include');
+    $p->withVariable('LDFLAGS', '$LDFLAGS -L' . $libffi_prefix . '/lib');
+    $p->withVariable('LIBS', '$LIBS -lffi');
 };
