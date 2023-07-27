a78d84d2ba369720b2b3743e9eacfb538163c361
wenjie <w@tryto.cn>
2023-07-27 18:27:55 +0800

    ext ffi

--- sapi/src/builder/library/libffi.php
+++ sapi/src/builder/library/libffi.php
@@ -12,23 +12,36 @@ return function (Preprocessor $p) {
             ->withUrl('https://github.com/libffi/libffi/releases/download/v3.4.4/libffi-3.4.4.tar.gz')
             ->withFile('libffi-3.4.4.tar.gz')
             ->withPrefix($libffi_prefix)
-            ->withConfigure(
+            //             ->withConfigure(
+            //                 <<<EOF
+            //                 ./configure --help
+            //                 ./configure \
+            //                 --prefix={$libffi_prefix} \
+            //                 --disable-docs \
+            //                 --enable-static=yes \
+            //                 --enable-shared=no
+            // EOF
+            //             )
+            ->withBuildScript(
                 <<<EOF
-                ./configure --help
-                ./configure \
-                --prefix={$libffi_prefix} \
-                --disable-docs \
-                --enable-static=yes \
-                --enable-shared=no
-EOF
+                mkdir -p build
+                cd build
+                cmake .. \
+                -DCMAKE_INSTALL_PREFIX={$libffi_prefix} \
+                -DCMAKE_BUILD_TYPE=Release  \
+                -DBUILD_STATIC_LIBS=ON \
+                -DBUILD_SHARED_LIBS=OFF \
+                -DBUILD_TESTING=OFF
+                cmake --build . --target install
+            EOF
             )
             ->withPkgName('libffi')
-            ->withPkgConfig($libffi_prefix . '/lib/pkgconfig')
-            ->withLdflags('-L' . $libffi_prefix . '/lib/')
+            // ->withPkgConfig($libffi_prefix . '/lib/pkgconfig')
+            // ->withLdflags('-L' . $libffi_prefix . '/lib/')
             ->withBinPath($libffi_prefix . '/bin/')
     );
-    $p->withVariable('CPPFLAGS', '$CPPFLAGS -DFFI_BUILDING_DLL -I' . $libffi_prefix . '/include');
-    $p->withVariable('LDFLAGS', '$LDFLAGS -L' . $libffi_prefix . '/lib');
-    $p->withVariable('CPP', '$CPP cl -nologo -EP');
+    // $p->withVariable('CPPFLAGS', '$CPPFLAGS -DFFI_BUILDING_DLL -I' . $libffi_prefix . '/include');
+    // $p->withVariable('LDFLAGS', '$LDFLAGS -L' . $libffi_prefix . '/lib');
+    // $p->withVariable('CPP', '$CPP cl -nologo -EP');
     // $p->withVariable('LIBS', '$LIBS -lffi');
 };
