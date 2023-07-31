0b080cdcff1e27e8bc402fa5798915fca5e0609d
wenjie <w@tryto.cn>
2023-07-31 14:55:30 +0800

    cygwin swow

--- sapi/scripts/cygwin/cygwin-config-swow.sh
+++ sapi/scripts/cygwin/cygwin-config-swow.sh
@@ -22,50 +22,55 @@ cd ${__PROJECT__}/php-src/
 # export CFLAGS=""
 # export LDFLAGS="-L/usr/lib"
 
+php prepare.php -bcmath -bz2 -ctype -curl -exif -fileinfo -filter -gd -gmp -iconv -imagick -intl -mbstring -mongodb -mysqli -mysqlnd -opcache -openssl -pcntl -pdo -pdo_mysql -pdo_sqlite -phar -posix -readline -redis -session -soap -sockets -sodium -sqlite3 -swoole -tokenizer -xml -xsl -yaml -zip -zlib
+
 ./buildconf --force
 test -f Makefile && make clean
-./configure --prefix=/usr --disable-all \
-  --disable-fiber-asm \
-  --enable-opcache \
-  --without-pcre-jit \
-  --with-openssl --enable-openssl \
-  --with-curl \
-  --with-iconv \
-  --enable-intl \
-  --with-bz2 \
-  --enable-bcmath \
-  --enable-filter \
-  --enable-session \
-  --enable-tokenizer \
-  --enable-mbstring \
-  --enable-ctype \
-  --with-zlib \
-  --enable-posix \
-  --enable-sockets \
-  --enable-pdo \
-  --with-sqlite3 \
-  --enable-phar \
-  --enable-pcntl \
-  --enable-mysqlnd \
-  --with-mysqli \
-  --enable-fileinfo \
-  --with-pdo_mysql \
-  --with-pdo-sqlite \
-  --enable-soap \
-  --with-xsl \
-  --with-gmp \
-  --enable-exif \
-  --with-sodium \
-  --enable-xml --enable-simplexml --enable-xmlreader --enable-xmlwriter --enable-dom --with-libxml \
-  --enable-gd --with-jpeg --with-freetype \
-  --enable-swow --enable-swow-ssl --enable-swow-curl \
-  --enable-redis \
-  --with-imagick \
-  --with-yaml \
-  --with-readline \
-  --with-pdo-pgsql \
-  --with-pgsql \
-  --with-ffi
+
+./configure --prefix=/usr --disable-all --enable-swow
+
+# ./configure --prefix=/usr --disable-all \
+#   --disable-fiber-asm \
+#   --enable-opcache \
+#   --without-pcre-jit \
+#   --with-openssl --enable-openssl \
+#   --with-curl \
+#   --with-iconv \
+#   --enable-intl \
+#   --with-bz2 \
+#   --enable-bcmath \
+#   --enable-filter \
+#   --enable-session \
+#   --enable-tokenizer \
+#   --enable-mbstring \
+#   --enable-ctype \
+#   --with-zlib \
+#   --enable-posix \
+#   --enable-sockets \
+#   --enable-pdo \
+#   --with-sqlite3 \
+#   --enable-phar \
+#   --enable-pcntl \
+#   --enable-mysqlnd \
+#   --with-mysqli \
+#   --enable-fileinfo \
+#   --with-pdo_mysql \
+#   --with-pdo-sqlite \
+#   --enable-soap \
+#   --with-xsl \
+#   --with-gmp \
+#   --enable-exif \
+#   --with-sodium \
+#   --enable-xml --enable-simplexml --enable-xmlreader --enable-xmlwriter --enable-dom --with-libxml \
+#   --enable-gd --with-jpeg --with-freetype \
+#   --enable-swow --enable-swow-ssl --enable-swow-curl \
+#   --enable-redis \
+#   --with-imagick \
+#   --with-yaml \
+#   --with-readline \
+#   --with-pdo-pgsql \
+#   --with-pgsql \
+#   --with-ffi
 
 #   --with-zip   #  cygwin libzip-devel 版本库暂不支持函数 zip_encryption_method_supported （2020年新增函数)
 
